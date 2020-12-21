ad3a23034ffff6bf0548caec95d99276bd865f8c
Sebas <sebas.warsaw@gmail.com>
2020-12-21 08:05:48 -0300

    new engine

--- conway.js
+++ conway.js
@@ -1,8 +1,15 @@
 var express = require("express"),
   morgan = require("morgan"),
+  bodyParser = require("body-parser"),
   app = express(),
   port = process.env.PORT || 3002,
   router = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("views", __dirname + "/public");
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
 app.use("/conway", express.static(__dirname + "/views")); // set the static files location for the static html
 app.use("/conway", express.static(__dirname + "/public")); // set the static files location /public/img will be /img for users
